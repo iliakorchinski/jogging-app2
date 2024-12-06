1d4fdccc74fd6f67c250cf9e45cdf4a2d29d946f
Ilya Kortchinski <ilyakortchinski@Ilyas-MacBook-Pro.local>
2024-12-06 12:40:48 +0300

    add filter by date and some fixes in styling

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router';
 import MainNav from '../components/MainNav/MainNav';
 import { useState } from 'react';
-import BurgerMenu from '../BurgerMenu/BurgerMenu';
+import BurgerMenu from '../components/BurgerMenu/BurgerMenu';
 
 export default function RootLayout() {
   const [isActiveMenu, setIsActiveMenu] = useState(false);
